49f5c7cd8383091ff08823311180e62c21f70c4c
Joshua005 <patogallina1987@gmail.com>
2024-06-27 11:18:16 -0400

    sinApi

---
+++ script.js
@@ -0,0 +1,70 @@
+let intentos = 6
+let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH']
+const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+const button = document.getElementById("guess-button")
+
+
+window.addEventListener('load', init)
+button.addEventListener("click", intenar)
+
+
+
+
+function intenar(){
+    const INTENTO = leerIntento();
+    if(INTENTO === palabra){
+        terminar("<h1>GANASTE! :)</h1>")
+        
+    }
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
+    for (let i in palabra){
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        SPAN.style.borderRadius = '5px'
+        if (INTENTO[i]===palabra[i]){ 
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'green';
+        } else if( palabra.includes(INTENTO[i]) ) {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'yellow';
+        } else { 
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'grey';
+        }
+        ROW.appendChild(SPAN)
+    }
+GRID.appendChild(ROW)
+intentos--
+if (intentos==0){
+    terminar("<h1>PERDISTE!</h1>")
+}
+
+}
+
+function terminar(mensaje){
+    const INPUT = document.getElementById("guess-input")
+    const button = document.getElementById("guess-button")
+    INPUT.disabled = true
+    button.disabled = true;
+    let contenedor = document.getElementById("guesses")
+    contenedor.innerHTML = mensaje
+}
+
+
+
+
+function leerIntento(){
+    let intento = document.getElementById("guess-input")
+    intento = intento.value
+    intento = intento.toUpperCase()
+    return intento    
+}
+
+
+function init(){
+    console.log('Esto se ejecuta solo cuando se carga la pagina web')
+    console.log(palabra)
+}
+
